4e993e1c67a10498b46223cde17b6b2bb1ac8bd9
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-05-06 10:27:44 -0300

    go

--- go/hello.go
+++ go/hello.go
@@ -1,7 +1,31 @@
+// Declaração do pacote principal
 package main
 
-import "fmt"
+// Importa os pacotes necessários para o programa
+import (
+	"fmt"      // Pacote para formatação de texto e saída
+	"net/http" // Pacote para lidar com solicitações HTTP
+)
+
+// handler é uma função que lida com requisições HTTP
+func handler(w http.ResponseWriter, r *http.Request) {
+	// Serve o arquivo index.html
+	http.ServeFile(w, r, "index.html")
+
+	// Imprime o endereço remoto da requisição
+	fmt.Println("Acesso à porta 8080 de:", r.RemoteAddr)
+}
 
 func main() {
-	fmt.Println("Bem-Vindo ao Hello World GO da ToolBox Playground!")
+	// Imprime a URL do servidor
+	fmt.Println("Agora escutando em: http://localhost:8080")
+
+	// Registra a função handler para lidar com requisições para o caminho raiz "/"
+	http.HandleFunc("/", handler)
+
+	// Serve arquivos estáticos do diretório "/static/"
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+
+	// Inicia o servidor HTTP e escuta na porta 8080
+	http.ListenAndServe(":8080", nil)
 }
