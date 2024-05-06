7b184a64b277dd5e01b67046f938aec75b374186
Marcelo Buzzetti <marcelobuzzetti@gmail.com>
2024-05-06 15:05:54 -0300

    templates

--- python/app.py
+++ python/app.py
@@ -1,10 +1,10 @@
-from flask import Flask  # Importe o módulo Flask
+from flask import Flask, render_template  # Importe o módulo Flask
 
 app = Flask(__name__)  # Crie uma aplicação Flask
 
 @app.route('/')  # Defina uma rota para a URL raiz ("/")
 def hello_world():  # Defina uma função para lidar com a requisição da URL raiz
-    return 'Bem-Vindo ao Hello World Flask Python'  # Retorne a string "Hello, World!" como resposta
+    return render_template('index.html')  # Renderiza o template index.html
 
 if __name__ == '__main__':  # Verifique se o script está sendo executado diretamente (não importado)
     app.run(host="127.0.0.1", port=5000, debug=True) # Inicie a aplicação Flask
