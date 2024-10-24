b155177a000a723e75fae427da2618ed44700486
Luis Felipe <luisdeol@hotmail.com>
2024-10-24 15:25:07 -0300

    adding endpoints

---
+++ dotnet/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace dotnet.Models
+{
+    public class Customer
+    {
+        public Customer(int id, string name, DateTime birthDate)
+        {
+            Id = id;
+            Name = name;
+            BirthDate = birthDate;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,3 +1,5 @@
+using dotnet.Models;
+
 var builder = WebApplication.CreateBuilder(args); // Cria uma instância do WebApplicationBuilder usando os argumentos fornecidos
 
 var app = builder.Build(); // Constrói a aplicação web com base na instância do WebApplicationBuilder
@@ -5,5 +7,24 @@ var app = builder.Build(); // Constrói a aplicação web com base na instância
 app.UseStaticFiles(); // Configura a aplicação para usar arquivos estáticos, como arquivos CSS, JavaScript e imagens
 
 app.MapGet("/", () => Results.Redirect("/index.html")); // Mapeia a rota raiz ("/") para redirecionar para a página "index.html"
+
+app.MapGet("/api/customers", () =>
+{
+    Customer[] customers = [ new Customer(1, "Thiago", DateTime.Now.AddYears(-30)) ];
+
+    return Results.Ok(customers);
+});
+
+app.MapGet("/api/customers/{id}", (int id) =>
+{
+    Customer[] customers = [new Customer(1, "Thiago", DateTime.Now.AddYears(-30))];
+
+    var customer = customers.SingleOrDefault(c => c.Id == id);
+
+    if (customer is null)
+        return Results.NotFound();
+
+    return Results.Ok(customer);
+});
 
 app.Run(); // Executa a aplicação web
